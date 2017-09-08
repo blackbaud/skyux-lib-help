c7de8063623c6962497221e0df5a2edf8925c013
bdjones2410 <brandon.jones@blackbaud.com>
2017-09-08 07:45:25 -0400

    removed dependencies, added direct dependency on help client

--- src/app/public/src/bb-help.module.ts
+++ src/app/public/src/bb-help.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 
-const objectAssign = require('es6-object-assign');
-objectAssign.polyfill();
-
 import { HelpKeyModule } from './modules/help-key';
 import { BBHelpSharedModule } from './modules/shared';
 import { TestButtonModule } from './modules/test-button';
