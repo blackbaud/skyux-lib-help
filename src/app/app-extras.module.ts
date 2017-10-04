436e7cfd3caf101997cafb121d1b3f28dc5e2e5a
bdjones2410 <brandon.jones@blackbaud.com>
2017-10-04 07:59:43 -0400

    merge conflict

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 
 import { BBHelpModule } from './public';
 
-<<<<<<< HEAD
 import { SkyModalDemoFormComponent } from './lib/modal-demo/modal-demo-form.component';
-=======
 import { HelpWindowRef } from './local-shared/window-ref';
->>>>>>> 6e2bfd605dd39ac4a3ac9945d0d170de58d7b970
 
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
