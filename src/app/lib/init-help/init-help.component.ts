bbb9186a3828a271a0a1c5defce5ad2ae79c1af3
Blackbaud-TimPepper <tim.pepper@blackbaud.com>
2019-05-16 13:56:57 -0400

    update help client version

--- src/app/lib/init-help/init-help.component.ts
+++ src/app/lib/init-help/init-help.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
 import { HelpInitializationService } from '../../public';
 
-import { HelpWindowRef } from '../window-ref';
-
 @Component({
   selector: 'init-help',
   template: ''
 })
 export class HelpInitComponent {
   constructor(
-    private windowRef: HelpWindowRef,
     private initService: HelpInitializationService) {
-    if (!this.windowRef.nativeWindow.BBHELP) {
-      this.initService.load({
-        extends: 'bb-help'
-      });
-    }
+      this.initService.load({ extends: 'bbhelp' });
   }
 }
--- src/app/public/src/modules/shared/initialization.service.ts
+++ src/app/public/src/modules/shared/initialization.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BBHelpClient } from '@blackbaud/help-client';
-
 import { HelpWidgetConfig } from './widget-config';
 
 @Injectable()
 export class HelpInitializationService {
-
   public load(config: HelpWidgetConfig) {
-    BBHelpClient.load(config);
+    BBHelpClient.ready()
+      .then(() => {
+        BBHelpClient.load(config);
+      });
   }
 }
