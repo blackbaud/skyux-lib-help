d25063f0a145e33a519649b30d4a03e26753e587
Colby M. White <blackbaud-colbywhite@users.noreply.github.com>
2021-09-02 15:38:21 -0500

    feat: init widget same way consumers would

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,33 +1,55 @@
 import {
+  APP_INITIALIZER,
   NgModule
 } from '@angular/core';
 
 import {
-  BBHelpModule,
-  HelpInitializationService
-} from './public/public_api';
+  SkyAppConfig
+} from '@skyux/config';
 
 import {
   SkyModalDemoFormComponent,
   HelpWindowRef
 } from './lib';
 
-// Specify entry components, module-level providers, etc. here.
 import {
   AppSkyModule
 } from './app-sky.module';
 
+import {
+  BBHelpModule,
+  HelpWidgetService,
+  HelpInitializationService
+} from './public/public_api';
+
+/**
+ * Loads the widget via {@link HelpInitializationService} in similar fashion as `@skyux-sdk/builder`.
+ * This is done manually for the local demo in order to differentiate between the local classes from the classes that
+ * `@skyux-sdk/builder` is using.
+ * Consumers should not mimic this strategy.
+ */
+function initFunction(initSvc: HelpInitializationService, appConfig: SkyAppConfig) {
+  return () => initSvc.load(appConfig?.skyux?.help);
+}
+
+/**
+ * Consumers' version of {@link AppExtrasModule} would normally not contain
+ * {@link BBHelpModule}, {@link HelpWidgetService}, or {@link HelpInitializationService}.
+ * `@skyux-sdk/builder` would bring each in when the consumer provides a config object (even an empty object) in the
+ * `skyuxconfig.json`'s `help` property.
+ * This is done here in order to differentiate between the local classes from the classes that `@skyux-sdk/builder` is
+ * using.
+ */
 @NgModule({
-  imports: [
-    BBHelpModule
-  ],
   exports: [
     AppSkyModule,
     BBHelpModule
   ],
   providers: [
     HelpWindowRef,
-    HelpInitializationService
+    HelpWidgetService,
+    HelpInitializationService,
+    {provide : APP_INITIALIZER, useFactory: initFunction, deps: [HelpInitializationService, SkyAppConfig] , multi : true}
   ],
   entryComponents: [
     SkyModalDemoFormComponent
