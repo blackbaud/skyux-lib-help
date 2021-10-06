bbe6b54a2c28336195e439e21dca30a16e8f9918
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-10-06 12:40:13 -0400

    Adopt Angular CLI

---
+++ projects/help/src/public-api.ts
@@ -0,0 +1,18 @@
+export * from './modules/help/help.module';
+
+export * from './modules/help-directive/disable.module';
+
+export * from './modules/help-key/help-key.module';
+
+export * from './modules/open-on-click-directive/open-on-click.module';
+
+export * from './modules/shared/initialization.service';
+export * from './modules/shared/widget.service';
+export * from './modules/shared/widget-config';
+
+export * from './bb-help.module';
+
+export * from './modules/help/help.component';
+export * from './modules/help-directive/disable.directive';
+export * from './modules/help-key/help-key.component';
+export * from './modules/open-on-click-directive/open-on-click.directive';
